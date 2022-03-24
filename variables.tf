9fe5f04f9e9ad921beca56984f0484a5686c46aa
SurendraReddyWesco <98414596+SurendraReddyWesco@users.noreply.github.com>
2022-03-24 16:49:21 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "cluster_name" {
+  description = "The name for the AKS cluster"
+}
+variable "env_name" {
+  description = "The environment for the AKS cluster"
+}
+variable "instance_type" {
+  description = "instance type for the AKS cluster"
+}
